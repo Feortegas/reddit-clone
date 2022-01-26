cd595c595d6a0eddfa0f0d1e4db7c758ad20286b
Jojo Bautista <gjojob@yahoo.com>
2022-01-26 16:35:57 -0600

    test

--- public/javascript/thumbs.js
+++ public/javascript/thumbs.js
@@ -1,24 +1,15 @@
 var post_id;
-var upBtns = document.getElementsByClassName('thumbs-up-btn');
-Array.from(upBtns).forEach (element => {
-  element.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    post_id = element.dataset.postid;
+// var upBtns = document.getElementsByClassName('thumbs-up-btn');
+document.querySelector('.post-list').addEventListener('click', event => {
+  if(event.target.className === 'bi bi-hand-thumbs-up'){
+    post_id = event.target.getAttribute("data-postid");
     addThumbs(true);
-  });
+  } else if (event.target.className === 'bi bi-hand-thumbs-down') {
+    post_id = event.target.getAttribute("data-postid");
+    addThumbs(false);
+  }
 });
 
-var downBtns = document.getElementsByClassName('thumbs-down-btn');
-Array.from(downBtns).forEach (element => {
-  element.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    post_id = element.dataset.postid;
-    addThumbs(false);
-  });
-});
-   
 async function addThumbs(thumbsUp){
   try {
     const response = await fetch('/api/posts/thumbs', {
@@ -32,7 +23,8 @@ async function addThumbs(thumbsUp){
       }
     });
     console.log(post_id);
-    alert('test');
+    console.log(thumbsUp);
+    alert('check');
     document.location.reload();
 
   } catch(err) {
