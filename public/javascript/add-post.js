b3aab73365996d6df65e8e6a7710782075ff16a9
Jojo Bautista <gjojob@yahoo.com>
2022-01-28 00:12:33 -0600

    Added toggleable tabs when adding posts for text, image and links

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -22,4 +22,27 @@ async function newPostHandler(event) {
   }
 }
 
+// https://www.w3schools.com/howto/howto_js_tabs.asp
+// Toggleable Tabs
+function showForm(e, tabName) {
+  // Declare all variables
+  var i, tabContents, tabLinks;
+
+  // Get all elements with class="tabContents" and hide them
+  tabContents = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+  }
+
+  // Get all elements with class="tabLinks" and remove the class "active"
+  tabLinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  e.currentTarget.className += " active";
+}
+
 document.querySelector('.new-post-form').addEventListener('submit', newPostHandler);
