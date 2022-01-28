26715405e18306e00e27975a9bea10424dfa8ac4
Fernando Ortega <feortegas@gmail.com>
2022-01-28 00:01:24 -0600

    fix issue with username on comments page

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,7 +4,7 @@ async function newPostHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_content = document.querySelector('textarea[name="post-text"]').value;
 
-  const response = await fetch(`/api/posts`, {
+  const response = await fetch('/api/posts', {
     method: 'POST',
     body: JSON.stringify({
       title,
@@ -16,7 +16,8 @@ async function newPostHandler(event) {
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard');
+    // document.location.replace('/dashboard');
+    document.location.reload();
   } else {
     alert(response.statusText);
   }
