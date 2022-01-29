73a8de7da30e7f6cfe36ba1121fb250373903b97
Jojo Bautista <gjojob@yahoo.com>
2022-01-29 01:07:40 -0600

    Added functionality of pasting image from the clipboard and show the preview in the image tab

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,4 +1,4 @@
-async function newPostHandler(event) {
+async function newTextPostHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
@@ -22,6 +22,50 @@ async function newPostHandler(event) {
   }
 }
 
+async function newImagePostHandler(event) {
+  event.preventDefault();
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document.querySelector('textarea[name="post-text"]').value;
+
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      post_content
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    console.log(response.statusText);
+  }
+}
+
+document.onpaste = (e) => {
+  const imgTabActive = document.querySelector('#image-form').style.display;
+  // Make sure clipboard is not empty and Image Tab is currently selected
+  if (e.clipboardData.files.length > 0 && imgTabActive === 'block') {
+    const fileInput = document.querySelector('#image-file');
+    fileInput.files = e.clipboardData.files;
+    // Check the recent content of clipboard if it is an image file
+    if (e.clipboardData.files[0].type.startsWith("image/")) {
+      previewImage(e.clipboardData.files[0]);
+    }
+  }
+};
+
+async function previewImage(file) {
+  var fileReader = new FileReader();
+  fileReader.readAsDataURL(file);
+  fileReader.onload = () => {
+    document.querySelector('#image-preview').src = fileReader.result;
+  };
+}
+
 // Toggleable Tabs
 function showForm(e, tabName) {
   // Get all elements with class="tabContents" and hide them
@@ -41,4 +85,6 @@ function showForm(e, tabName) {
   e.currentTarget.className += " active";
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newPostHandler);
+document.querySelector('#text-form').addEventListener('submit', newTextPostHandler);
+document.querySelector('#image-form').addEventListener('submit', newImagePostHandler);
+// document.querySelector('#file-preview').addEventListener('change', previewImageFile);
